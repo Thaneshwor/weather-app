1006738787e9ef621ac144cf9838ebbb142d7fc9
Thaneshwor <thaneshwor100@gmail.com>
2020-04-06 16:42:50 +0545

    Updates

---
+++ precache-manifest.f71990f1a9a4b70c86689bcd6cb79cea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e3b2a5df314c4bf33f2beecfd7cd1e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b667fe1f6ee7240cd947",
+    "url": "/static/css/main.5ddc032c.chunk.css"
+  },
+  {
+    "revision": "b2ec0a2ffe7f33b44643",
+    "url": "/static/js/2.85fc8bd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.85fc8bd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b667fe1f6ee7240cd947",
+    "url": "/static/js/main.0655301d.chunk.js"
+  },
+  {
+    "revision": "e6462031e290b434c8f4",
+    "url": "/static/js/runtime-main.beb8c348.js"
+  },
+  {
+    "revision": "485a4520e12aa5cdb60a1aabd47456fa",
+    "url": "/static/media/weather.485a4520.jpeg"
+  }
+]);
